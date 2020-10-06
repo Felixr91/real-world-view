06ab3b599742e7e277181ace88ccc6e245e0d18a
Felix <felix@Megans-MBP-2.hitronhub.home>
2020-10-06 14:32:07 -0600

    Axios installed

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -4,7 +4,7 @@
 
     <h1>Event Listings</h1>
     <EventCard/>
-    <BaseIcon/>
+    <BaseIcon name='activity'/>
     <BaseInput/>
 
     
@@ -18,7 +18,7 @@
 
 import EventCard from '@/components/EventCard.vue'
 
-// BELOW local registering it as a component of EventList.vue
+// BELOW local registering of EventCard it as a component of EventList.vue
 // This is great but if we wanted EventCard.vue to be registered in more than one componenent we would have to import in every component that needed it. 
 // To do that you have to register it Globally. Base components are shared throughout your app frequently - things like base icons or base buttons.
 export default {
