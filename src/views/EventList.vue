03a28d2043f7c050a2487999214d90c87b07de59
Felix <felix@Megans-MBP-2.hitronhub.home>
2020-10-06 15:03:19 -0600

    db

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -15,20 +15,30 @@
 
 
 <script>
-
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 
-// BELOW local registering of EventCard it as a component of EventList.vue
-// This is great but if we wanted EventCard.vue to be registered in more than one componenent we would have to import in every component that needed it. 
-// To do that you have to register it Globally. Base components are shared throughout your app frequently - things like base icons or base buttons.
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events') 
+      .then(response => {
+        this.events = response.data // <--- set the events data
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response)
+      })
   }
 }
-
 </script>
-
 
 
 
